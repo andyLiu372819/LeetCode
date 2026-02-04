23ca56f419952a6982f2af7ea5e703f3213d9fcc
Andy Liu <andyl142568@gmail.com>
2026-02-03 22:47:43 -0500

    fixed some more mostakes

--- DividingTwoInteger.py
+++ DividingTwoInteger.py
@@ -1,19 +1,22 @@
 class Solution:
-    def divide(self, dividend: int, divisor: int) -> int:\
-        # Try division by repeated subtraction
-        def div_by_sub(N, D):
-            if (N < 0):
-                return -div_by_sub(-N, D)
-            elif (D < 0):
-                return -div_by_sub(N, -D)
-            else:
-                Q = 0
-                while N >= D:
-                    N -= D
-                    Q += 1
-                return Q
-        return div_by_sub(dividend, divisor)
+    def divide(self, dividend: int, divisor: int) -> int:
+        Q = 0
+
+        negative = (dividend < 0 or divisor < 0) and not (dividend < 0 and divisor < 0)
+        dividend, divisor = abs(dividend), abs(divisor)
+
+        while dividend >= divisor:
+            i = 0
+            while dividend >= (divisor << (i + 1)):
+                i += 1
+
+            dividend = dividend - (divisor << i)
+            Q += (1 << i)
+            
+
+        return Q if not negative else -Q
 
 
 sol = Solution()
-print(sol.divide(10, 3))  # Output: 3
+print(sol.divide(-1, -1))  # Output: 3
+
